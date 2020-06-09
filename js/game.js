cc8a65f046cec4f97a4ee464f15d841e3835400d
Libby <libby@users-mbp-698.lan>
2020-06-09 17:15:06 +0100

    init commit, need to do cursor movement

--- js/game.js
+++ js/game.js
@@ -59,6 +59,8 @@ Game.create = function(){
 //  game.input.setDraggable([ cold, mono, herpes, sars, measles ]);
 //  game.input.dragDistanceThreshold = 100;
 
+//  game.input.setDefaultCursor('cursor');
+
     Game.playerMap = {};
     var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     testKey.onDown.add(Client.sendTest, this);
@@ -71,12 +73,16 @@ Game.create = function(){
     //     layer = map.createLayer(i);
     // }
     // layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
+
+
     // layer.events.onInputUp.add(Game.getCoordinates, this);
+    Game.getCoordinates();
     Client.askNewPlayer();
+  //  Game.movePlayer();
 };
 
-Game.getCoordinates = function(layer,pointer){
-    Client.sendClick(pointer.worldX,pointer.worldY);
+Game.getCoordinates = function(pointer){
+    Client.sendClick(game.input.mousePointer.x,game.input.mousePointer.y);
 };
 
 Game.addNewPlayer = function(id,x,y){
@@ -85,11 +91,14 @@ Game.addNewPlayer = function(id,x,y){
 
 Game.movePlayer = function(id,x,y){
     var player = Game.playerMap[id];
+
+/*
+
     var distance = Phaser.Math.distance(player.x,player.y,x,y);
     var tween = game.add.tween(player);
     var duration = distance*10;
     tween.to({x:x,y:y}, duration);
-    tween.start();
+    tween.start();  */
 };
 
 Game.removePlayer = function(id){
